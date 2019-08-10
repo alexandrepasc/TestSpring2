702c7285ac4cd1224cc8068f4f360dc680564527
alexandrepasc <alexandre.pascoal@gmail.com>
2019-08-10 20:12:34 +0100

    1st endpoint
    Create controller.
    Comment some dependencies, for this stage.

--- src/main/java/com/TestSpring2/Spring2App.java
+++ src/main/java/com/TestSpring2/Spring2App.java
@@ -1,4 +1,12 @@
 package com.TestSpring2;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Spring2App {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(Spring2App.class, args);
+  }
 }
